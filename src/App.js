27509c5da2be20bcad7f8bb1345c6de16f006b3b
divas <divas@divass-MacBook-Air.local>
2022-02-20 01:42:15 +0530

    product and rating component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import data from './data';
- 
+import Product from './components/Product'; 
 
 export default function App (){
     return (
@@ -26,44 +26,7 @@ export default function App (){
                         {
                             data.products.map (
                                 (product)=>(
-                                <div key =  {product._id }className="card">
-                                <a href={`/products/${product._id}`}>
-                                    <img className="medium" src = {product.image} alt="product"/>
-
-                                </a>
-                                <div className="card-body">
-                                    <a href={`/products/${product._id}`}>
-                                    <h2 >{product.name}</h2>
-                                    </a>
-                                    <div className = "rating">
-                                        <span>
-                                            
-                                            <i className="fa fa-star"></i>
-                                        </span>
-                                        <span>
-                                            
-                                            <i className="fa fa-star"></i>
-
-                                        </span>
-                                        <span>
-                                            
-                                            <i className="fa fa-star"></i>
-
-                                        </span>
-                                        <span>
-                                            
-                                            <i className="fa fa-star"></i>
-
-                                        </span>
-                                        <span>
-                                            
-                                            <i className="fa fa-star"></i>
-
-                                        </span>
-                                    </div>
-                                    <div className='price'>rs {product.price}</div>
-                                </div>
-                                </div>
+                                    <Product key={product._id} product = {product}/>
                                 )
                             ) 
                         }
